e5eac3e8b08790b76a7e1446176aeb630ca80b09
Jeremy Wright <jeremy.wright@salesforce.com>
2023-06-06 12:12:20 -0300

    Add a basic example landing page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { Plugins } from '@capacitor/core';
+
+const { Haptics } = Plugins;
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  async handleButtonClick() {
+    if (Capacitor.isPluginAvailable('Haptics')) {
+      await Haptics.vibrate();
+    }
+  }
 }
+
